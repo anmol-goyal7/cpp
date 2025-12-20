53c182dbb1cc7a36206a606e2ab24cca0dcd035a
anmol <anmolafk7@gmail.com>
2025-12-20 21:53:02 +0530

    sum

--- basics/hello.cpp
+++ basics/hello.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <limits>
 
 int main() {
-	int x = 10;
-	int y = 20;
+    int x = 10;
+    int y = 20;
 
-	std::cout << "Sum = " << x + y << '\n';
-return 0;
+    std::cout << "Sum = " << x + y << '\n';
+
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
+
+    return 0;
 }
 
---
+++ basics/hellp.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	int x = 10;
+	int y = 20;
+
+	std::cout << "Sum = " << x + y << '\n';
+return 0;
+}
