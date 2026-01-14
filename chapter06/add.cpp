5919e6a20a371f193821418ef75196e9c7dfdf43
anmol <anmolafk7@gmail.com>
2026-01-14 15:39:01 +0530

    commit

---
+++ chapter06/add.cpp
@@ -0,0 +1,10 @@
+#include "add.h"
+
+namespace BasicMath
+{
+
+int add(int x, int y)
+{
+return x + y;
+}
+}
---
+++ chapter06/main.cpp
@@ -0,0 +1,9 @@
+#include "add.h"
+#include<iostream>
+
+int main()
+{
+std::cout << BasicMath::add(4,3) << '\n';
+
+return 0;
+}
