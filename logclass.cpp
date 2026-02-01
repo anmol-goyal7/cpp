b0fb50c34b5b0f7c3b48fa76d2efd0c7ba5f0d06
anmol <anmolafk7@gmail.com>
2026-02-01 22:08:00 +0530

    commit

--- logclass.cpp
+++ logclass.cpp
@@ -1,18 +1,35 @@
 #include<iostream>
-int main()
-{
     class Log 
     {
     public:
+        const int LogLevelError = 0;
+        const int LogLevelWarning = 1;
+        const int LogLevelInfo = 2;
+    private:
+        int m_LogLevel;
+    public:
         void Setlevel(int level)
         {
-
+            m_LogLevel = LogLevelInfo ;
         }
 
         void Warn(const char* message)
         {
-
+            if (m_LogLevel >= LogLevelWarning)
+                std::cout << "[WARNING]: " << message << std::endl;
         }
+        void Info(const char* message)
+        {
+            if (m_LogLevel >= LogLevelInfo)
+                std::cout << "[INFO]: " << message << std::endl;
+        }
+        void Error(const char* message)
+        {
+            if (m_LogLevel >= LogLevelError)
+                std::cout << "[WARNING]: " << message << std::endl;
+        }
+
+
 
     };
 
@@ -20,7 +37,7 @@ int main()
     {
 
         Log log;
-        log.Setlevel(LogLevelWarning);
+        log.Setlevel(log.LogLevelWarning);
         log.Warn("Hello!");
         std::cin.get();
     }
