7028ced932a3d1aad815726bb734c28d3eab5a62
anmol <anmolafk7@gmail.com>
2026-02-03 19:25:33 +0530

    constructors

---
+++ chapter14/constructors.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+class Entity
+{
+    public: 
+        float X,Y;
+
+        Entity()
+        {
+            X= 0.0f;
+            Y= 0.0f;
+        }
+
+        void Print()
+        {
+            std::cout << X << ", " << Y << std::endl;
+        }
+};
+
+int main()
+{
+    Entity e;
+    std::cout << e.X << std::endl;
+    e.Print();
+
+    std::cin.get();
+}
